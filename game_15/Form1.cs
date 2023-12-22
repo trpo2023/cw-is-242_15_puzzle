4eafa6713847f12b16f8f679fb93b10f44c2fb48
LionBlade <ruslan.khisamutdinov@gmail.com>
2023-12-23 01:57:08 +0500

    2.0 Full

--- game_15/Form1.cs
+++ game_15/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 
-namespace game_15
+namespace  game_15
 {
    
     public partial class Form1 : Form
